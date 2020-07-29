9cd62862fc587f2d73342efcfe4eb550c45c7f9b
Matthew Consterdine <mrconsterdine@googlemail.com>
2020-07-30 00:06:12 +0100

    Better handle older browsers

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
 var $ = document.querySelectorAll.bind(document);
 
-// To allow multi-touch
-document.addEventListener('contextmenu', event => event.preventDefault());
-
-var waveform = 0;
-var tempo = $("#tempo")[0].value;
-var reverse = $("#reverse")[0].checked;
-var play = $("#play")[0].checked;
-
-var table = $("#table");
-var columns = $("#table > colgroup > col");
-var rows = $("#table > tbody > tr");
-
-var pointer = 0;
-
-var context = new (window.AudioContext || window.webkitAudioContext)();
-
-var oscillators = new Array(8);
-
-for(var i = 0; i < oscillators.length; i++) {
-	oscillators[i] = context.createOscillator();
-	oscillators[i].type = "sine";
-	oscillators[i].frequency.value = 440 + (i - oscillators.length / 2) * 20;
-	oscillators[i].playing = false;
-	oscillators[i].gain = context.createGain();
-	oscillators[i].gain.gain.value = 0.00001;
-	oscillators[i].connect(oscillators[i].gain);
-	oscillators[i].gain.connect(context.destination);
-	oscillators[i].start();
-}
-
 var Update = function(timestamp) {
 	if(!play) return;
 
@@ -56,13 +26,52 @@ var Update = function(timestamp) {
 	}
 }
 
-var intervalID = setInterval(Update, tempo);
+// To allow multi-touch
+document.addEventListener('contextmenu', function(event) {event.preventDefault()});
 
-$("#reverse")[0].addEventListener("change", e => reverse = e.target.checked);
-$("#tempo")[0].addEventListener("change", e => {
-	tempo = 60 / e.target.value * 1000;
-	clearInterval(intervalID);
-	intervalID = setInterval(Update, tempo);
-});
+var waveform = 0;
+var tempo = $("#tempo")[0].value;
+var reverse = $("#reverse")[0].checked;
+var play = $("#play")[0].checked;
 
-$("#play")[0].addEventListener("change", e => play = e.target.checked);
+var table = $("#table");
+var columns = $("#table > colgroup > col");
+var rows = $("#table > tbody > tr");
+
+var pointer = 0;
+
+try {
+	var context = new (window.AudioContext || window.webkitAudioContext)();
+
+	var oscillators = new Array(8);
+
+	for(var i = 0; i < oscillators.length; i++) {
+		oscillators[i] = context.createOscillator();
+		oscillators[i].type = "sine";
+		oscillators[i].frequency.value = 440 + (i - oscillators.length / 2) * 20;
+		oscillators[i].playing = false;
+		oscillators[i].gain = context.createGain();
+		oscillators[i].gain.gain.value = 0.00001;
+		oscillators[i].connect(oscillators[i].gain);
+		oscillators[i].gain.connect(context.destination);
+		oscillators[i].start();
+	}
+
+
+	var intervalID = setInterval(Update, tempo);
+
+	$("#reverse")[0].addEventListener("change", function(e) {
+		reverse = e.target.checked
+	});
+	$("#tempo")[0].addEventListener("change", function(e) {
+		tempo = 60 / e.target.value * 1000;
+		clearInterval(intervalID);
+		intervalID = setInterval(Update, tempo);
+	});
+
+	$("#play")[0].addEventListener("change", function(e) {
+		play = e.target.checked
+	});
+} catch(ignored) {
+	alert("AudioContext requires a modern browser");
+}
